9e3d1420e1cd6bc21a34f2e2afffe9004ce04bf7
Julius Caamic <julius@greenbrainer.com>
2016-03-22 12:19:29 +0800

    add list style checked to around the site

--- mysite/_config.php
+++ mysite/_config.php
@@ -11,3 +11,5 @@ require_once("conf/ConfigureFromEnv.php");
 i18n::set_locale('en_GB');
 
 FulltextSearchable::enable();
+
+HtmlEditorConfig::get('cms')->setOption('content_css', project() . '/css/editor.css');
